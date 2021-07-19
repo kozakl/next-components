47732d971921e9800f336059df3bfc1f0ddb6548
Luke Kozak <kozakluke@gmail.com>
2021-07-19 17:38:26 +0200

    Create nav-link component

---
+++ src/components/nav-link/NavLink.tsx
@@ -0,0 +1,34 @@
+import {AnchorHTMLAttributes,
+        FunctionComponent} from 'react';
+import {default as Link} from 'next/link';
+import {useRouter} from 'next/router';
+import {classNames} from '@kozakl/utils';
+
+const NavLink:FunctionComponent<Props> = (props)=>
+{
+    const {asPath} = useRouter();
+    const navLinkClass = classNames(
+        props.className,
+        props.active && props.activeClass ||
+            asPath.startsWith(props.href) &&
+                props.activeClass
+    );
+    return (
+        <Link href={props.href}>
+            <a
+                className={navLinkClass}
+                title={props.title}
+                onClick={props.onClick}>
+                {props.children}
+            </a>
+        </Link>
+    );
+};
+
+interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
+    className?:string;
+    activeClass?:string;
+    active?:boolean;
+}
+
+export default NavLink;
