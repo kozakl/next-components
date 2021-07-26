a8dc09f771bbea68d1efb6c24e18531bb46f6421
Luke Kozak <kozakluke@gmail.com>
2021-07-26 11:40:42 +0200

    Create home route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,3 @@
-import {FunctionComponent} from 'react';
-import {default as Head} from 'next/head';
+import {Home} from '../routes/home';
 
-const Index:FunctionComponent = ()=>
-{
-    return (
-        <div>
-            <Head>
-                <meta charSet="utf-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-                <meta name="theme-color" content="#E8E0BC"/>
-                <link rel="shortcut icon" href="/favicon.ico"/>
-                <link rel="manifest" href="/manifest.json"/>
-                
-                <title>Next Components</title>
-            </Head>
-            <main>
-                Next Components
-            </main>
-        </div>
-    );
-};
-
-export default Index;
+export default Home;
---
+++ src/routes/home/Home.tsx
@@ -0,0 +1,14 @@
+import {FunctionComponent} from 'react';
+import {Layout} from '../../components/layout';
+import style from './Home.module.css';
+
+const Home:FunctionComponent = ()=>
+{
+    return (
+        <Layout title="Camera Nera - Rental">
+            XXXX
+        </Layout>
+    );
+};
+
+export default Home;
