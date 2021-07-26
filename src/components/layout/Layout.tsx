8b21c01fc95a420ffb536b41eacd98295ede4d85
Luke Kozak <kozakluke@gmail.com>
2021-07-26 11:43:24 +0200

    Create layout component

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,35 @@
+import {FunctionComponent} from 'react';
+import {default as Head} from 'next/head';
+import {Toasts} from '../toasts';
+import {Toast} from '../../types';
+import style from './Layout.module.css';
+
+export const Layout:FunctionComponent<Props> = (props)=>
+{
+    return (
+        <div>
+            <Head>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+                <meta name="theme-color" content="#E8E0BC"/>
+                <link rel="shortcut icon" href="/favicon.ico"/>
+                <link rel="manifest" href="/manifest.json"/>
+                
+                <title>{props.title}</title>
+            </Head>
+            <main>
+                <div className={style.pages}>
+                    {props.children}
+                </div>
+                <Toasts toasts={props.toasts}/>
+            </main>
+        </div>
+    );
+};
+
+interface Props {
+    title?:string;
+    toasts:Toast[];
+}
+
+export default Layout;
