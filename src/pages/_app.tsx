2857cbb1113172c3dc242c32bc7d39393c393446
Luke Kozak <kozakluke@gmail.com>
2021-07-26 09:57:57 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,19 @@ import {FunctionComponent} from 'react';
 import {AppProps} from 'next/app'
 import '../main.css';
 
-const App:FunctionComponent<AppProps> = ({Component, pageProps}:AppProps)=>
-{
-    return <Component {...pageProps} />
+const App:FunctionComponent<AppProps> = ({Component, pageProps}:AppProps)=> {
+    return (
+        <>
+            <Component {...pageProps}/>
+            <div id="modal-root"/>
+        </>
+    )
 };
+
+if (typeof document !== 'undefined') {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+    window.addEventListener('resize', ()=>
+        document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`));
+}
 
 export default App;
