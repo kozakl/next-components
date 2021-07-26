65687a7e03fc02982e5ebb9b8ac9effe721bec3b
Luke Kozak <kozakluke@gmail.com>
2021-07-26 09:44:30 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,20 @@
+const {join} = require('path');
 const withTM = require('next-transpile-modules')([
-
+    
 ]);
 
 module.exports = withTM({
-    env: {
-        API: process.env.NODE_ENV === 'development' ?
-        'http://localhost' :
-        ''
+    typescript: {
+        ignoreBuildErrors: true,
     },
     webpack: (config)=> {
-        /*config.resolve = {
-            alias: {
-                res: join(__dirname, 'res')
-            }
-        };*/
+        config.resolve.alias['res'] = join(__dirname, 'res');
         config.module.rules.push({
             test: /res.*\.(woff|woff2|svg|png|jpg)$/,
             use: {
                 loader: 'url-loader',
                 options: {
-                    esModule: false,
-                    name: '[name].[ext]'
+                    esModule: false
                 }
             }
         });
